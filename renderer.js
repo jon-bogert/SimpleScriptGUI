236f2915cbef36941aff28f019f8db54066a18fa
Jon Bogert <jonbogert@gmail.com>
2025-10-05 14:57:48 -0700

    Sidebar Select

--- renderer.js
+++ renderer.js
@@ -1,40 +1,98 @@
 const { Project } = require('./project');
 
+let proj = null;
+let sceneIndex = -1;
+
+function renderEditor()
+{
+    const editorDiv = document.getElementById('editor');
+    editorDiv.innerHTML = '';
+
+    const pageTitle = document.getElementById('page-title');
+    if (proj === null || sceneIndex < 0)
+    {
+        pageTitle.innerText = "Select or Add a Sequence";
+        return;
+    }
+
+    let seq = proj.getSequence(sceneIndex);
+    pageTitle.innerText = seq.name;
+
+    for (const textBlock of seq.blocks)
+    {
+        const p = document.createElement('p');
+        p.textContent = (textBlock.type === 'Parenthetical') ? '(' + textBlock.content + ')' : textBlock.content;
+        
+        if (textBlock.type === 'Slug')
+        {
+            p.className = "slug-line";
+        }
+        else if (textBlock.type === 'Action')
+        {
+            p.className = "action-line";
+        }
+        else if (textBlock.type === 'Parenthetical')
+        {
+            p.className = 'parenthetical'
+        }
+        else if (textBlock.type === 'Dialogue')
+        {
+            p.className = 'dialogue'
+        }
+        editorDiv.appendChild(p);
+    }
+}
+
+function renderSidebar()
+{
+    const seqDiv = document.getElementById('sequence-list');
+    seqDiv.innerHTML = '';
+
+    if (proj === null)
+        return;
+
+    const seqCount = proj.getNumberOfSequences();
+    for (let i = 0; i < seqCount; ++i)
+    {
+        const p = document.createElement('p');
+
+        if (sceneIndex === i)
+        {
+            p.textContent = '> ' + proj.getSequence(i).name;
+            p.className = "sequence-entry-selected";
+        }
+        else
+        {
+            p.textContent = proj.getSequence(i).name;
+            p.className = "sequence-entry";
+        }
+
+
+        p.addEventListener('click', () => {
+            loadSequence(i);
+        });
+
+        seqDiv.appendChild(p);
+    }
+}
+
+function loadSequence(index)
+{
+    sceneIndex = index;
+    renderSidebar();
+    renderEditor();
+}
+
 async function displayProjects(path)
 {
-    let proj = new Project();
+    proj = new Project();
     
-        await proj.load('D:\\repos\\documentation\\ProjectEden_Script');
+    await proj.load(path);
         
-        const editorDiv = document.getElementById('editor');
-        editorDiv.innerHTML = '';
-
-        proj.forEach((textBlock) => {
-            const p = document.createElement('p');
-
-            p.textContent = (textBlock.type === 'Parenthetical') ? '(' + textBlock.content + ')' : textBlock.content;
-            
-            if (textBlock.type === 'Slug')
-            {
-                p.className = "slug-line";
-            }
-            else if (textBlock.type === 'Action')
-            {
-                p.className = "action-line";
-            }
-            else if (textBlock.type === 'Parenthetical')
-            {
-                p.className = 'parenthetical'
-            }
-            else if (textBlock.type === 'Dialogue')
-            {
-                p.className = 'dialogue'
-            }
-
-            editorDiv.appendChild(p);
-        });
+    renderSidebar();
+    renderEditor();
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    displayProjects('');
+    displayProjects('D:\\repos\\documentation\\ProjectEden_Script');
 });
